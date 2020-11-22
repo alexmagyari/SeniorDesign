7f61a82d6d638523a812fc70fd5153a8e6e12081
leslienix <leslienix@gmail.com>
2020-11-21 19:18:52 -0600

    updated to work with PWM

--- control/motorMix.c
+++ control/motorMix.c
@@ -19,14 +19,26 @@ m1 - Front Left  // CCW
 m2 - Back Right  // CCW
 m3 - Back Left   // CW
 *****************************************************/
+typedef struct{
+  float thrust, roll, pitch, yaw;
+  float m0, m1, m2, m3;
+  int m0_duty, m1_duty, m2_duty, m3_duty;
+} motor_mix;
 
-void compute_motor_commands(float thrust_cmd, float yaw_cmd, float pitch_cmd, float roll_cmd)
+
+#include "../embedded/motor.h"
+#include "IMU.h"
+#define MAX_MOTOR_OUT 1900
+#define MIN_MOTOR_OUT 1000
+void compute_motor_commands(motor_mix m_cmd, float thrust_cmd, float yaw_cmd, float pitch_cmd, float roll_cmd)
 {
-  float m0_cmd = thrust_cmd + yaw_cmd + pitch_cmd + roll_cmd;
-  float m1_cmd = thrust_cmd - yaw_cmd + pitch_cmd - roll_cmd;
-  float m2_cmd = thrust_cmd - yaw_cmd - pitch_cmd + roll_cmd;
-  float m3_cmd = thrust_cmd + yaw_cmd - pitch_cmd - roll_cmd;
+  m_cmd.m0 = thrust_cmd + yaw_cmd + pitch_cmd + roll_cmd;
+  m_cmd.m1 = thrust_cmd - yaw_cmd + pitch_cmd - roll_cmd;
+  m_cmd.m2 = thrust_cmd - yaw_cmd - pitch_cmd + roll_cmd;
+  m_cmd.m3 = thrust_cmd + yaw_cmd - pitch_cmd - roll_cmd;
 
-  // Output those commands to a place
-
+  m_cmd.m0_duty = m_cmd.m0 * (MAX_MOTOR_OUT - MIN_MOTOR_OUT);
+  m_cmd.m1_duty = m_cmd.m1 * (MAX_MOTOR_OUT - MIN_MOTOR_OUT);
+  m_cmd.m2_duty = m_cmd.m2 * (MAX_MOTOR_OUT - MIN_MOTOR_OUT);
+  m_cmd.m3_duty = m_cmd.m3 * (MAX_MOTOR_OUT - MIN_MOTOR_OUT);
 }
